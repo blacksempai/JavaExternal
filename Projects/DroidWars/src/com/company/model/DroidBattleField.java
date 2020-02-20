bae390f5b89c42d4bd398483355729bbfdcc5f1c
Andriy Moskalenko <benjaminfranclin1998@gmail.com>
2020-02-20 18:36:30 +0200

    Minor changes

--- Projects/DroidWars/src/com/company/model/DroidBattleField.java
+++ Projects/DroidWars/src/com/company/model/DroidBattleField.java
@@ -48,24 +48,24 @@ public class DroidBattleField {
         int round = 1;
 
         while (droid1.getBatteryLife() > 0 && droid2.getBatteryLife() > 0) {
-            fightResults.append(BattleInformationView.ROUND_MSG+round);
+            fightResults.append(BattleInformationView.ROUND_MSG+round+"\n");
             chanceToAimIntoDroid = rn.nextInt(100);
             if(rn.nextBoolean()) droid1.fly(); //If droid is lucky it can fly away before the attack
             if (chanceToAimIntoDroid > droid1.getVelocity()) {
                 if (rn.nextBoolean()) droid2.useSuperPower(droid1); //if droid is lucky it can use super power before attack
                 droid1.setBatteryLife(droid1.getBatteryLife() - droid2.getStrength());
-                fightResults.append(droid2.getDroidName() + BattleInformationView.HIT_MSG + (droid1.getBatteryLife()) + BattleInformationView.TO_MSG + droid1.getDroidName());
+                fightResults.append(droid2.getDroidName() + BattleInformationView.HIT_MSG + (droid1.getBatteryLife()) + BattleInformationView.TO_MSG + droid1.getDroidName()+"\n");
             } else {
-                fightResults.append(droid1.getDroidName()+BattleInformationView.AVOID_MSG);
+                fightResults.append(droid1.getDroidName()+BattleInformationView.AVOID_MSG+"\n");
             }
             chanceToAimIntoDroid = rn.nextInt(100);
             if(rn.nextBoolean()) droid2.fly();
             if (chanceToAimIntoDroid > droid2.getVelocity()) {
                 if (rn.nextBoolean()) droid1.useSuperPower(droid1);
                 droid2.setBatteryLife(droid2.getBatteryLife() - droid1.getStrength());
-                fightResults.append(droid1.getDroidName()+BattleInformationView.HIT_MSG+(droid2.getBatteryLife())+BattleInformationView.TO_MSG+droid2.getDroidName());
+                fightResults.append(droid1.getDroidName()+BattleInformationView.HIT_MSG+(droid2.getBatteryLife())+BattleInformationView.TO_MSG+droid2.getDroidName()+"\n");
             } else {
-                fightResults.append(droid2.getDroidName()+BattleInformationView.AVOID_MSG);
+                fightResults.append(droid2.getDroidName()+BattleInformationView.AVOID_MSG+"\n");
             }
             round++;
         }
